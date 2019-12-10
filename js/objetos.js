a2423569446f0aec3d79577438312cc7a8eda3c2
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2019-12-10 10:44:16 -0400

    añadiendo logo

--- js/objetos.js
+++ js/objetos.js
@@ -1,24 +1,39 @@
 // Object Literal
 const persona = {
-  nombre: "Juan",
-  profesion: "Desorrallador Web",
+  nombre: 'Juan',
+  profesion: 'Desorrallador Web',
   edad: 27
-};
-console.log(persona);
+}
+console.log(persona)
 
 // Object Constructor
 function Tarea(nombre, urgencia) {
-  this.nombre = nombre;
-  this.urgencia = urgencia;
+  this.nombre = nombre
+  this.urgencia = urgencia
 }
 
 // Crear una nueva tarea
-const tarea1 = new Tarea("Aprender JavaScript y react", "Urgente");
-const tarea2 = new Tarea("Preparar café", "Medio");
-const tarea3 = new Tarea("Pasear al perro", "Urgente");
-const tarea4 = new Tarea("Conocer a mis suegros", "bajo");
+const tarea1 = new Tarea('Aprender JavaScript y react', 'Urgente')
+const tarea2 = new Tarea('Preparar café', 'Medio')
+const tarea3 = new Tarea('Pasear al perro', 'Urgente')
+const tarea4 = new Tarea('Conocer a mis suegros', 'bajo')
 
-console.log(tarea1);
-console.log(tarea2);
-console.log(tarea3);
-console.log(tarea4);
+console.log(tarea1)
+console.log(tarea2)
+console.log(tarea3)
+console.log(tarea4)
+
+/**
+ * PROTOTYPES: son funciones atada al objeto que se pueden llamar
+ * No se envia las varibles en la function() ya que se esta utilizando el objeto Tarea
+ * Agregar un prototype a tarea
+ *
+ */
+
+Tarea.prototype.mostrarInformacionTarea = function() {
+  return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`
+}
+
+const tarea5 = new Tarea('Aprender NodeJS', 'Urgente')
+console.log(tarea5)
+console.log(tarea5.mostrarInformacionTarea())
